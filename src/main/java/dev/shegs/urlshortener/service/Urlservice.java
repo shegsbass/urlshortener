007acca990a58a787a893b99f04bb6b9b278e289
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-02-20 09:15:38 +0100

    Service Interface updated

--- src/main/java/dev/shegs/urlshortener/service/Urlservice.java
+++ src/main/java/dev/shegs/urlshortener/service/Urlservice.java
@@ -1,7 +1,15 @@
 package dev.shegs.urlshortener.service;
 
+import dev.shegs.urlshortener.model.Url;
+import dev.shegs.urlshortener.model.UrlDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface Urlservice {
+
+    public Url generateShortLink(UrlDto urlDto);
+    public Url persistShortLink(Url url);
+    public Url getEncodedUrl(String url);
+    public void deleteShortLink(Url url);
+
 }
